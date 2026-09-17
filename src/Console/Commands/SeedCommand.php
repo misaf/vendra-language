286dc2e8b8899424dcae9e8ef5520013691d95dd
misaf <misaf.1990@gmail.com>
2026-09-17 19:49:10 +0000

    test(store): cover the storefront:lifecycle command

--- src/Console/Commands/SeedCommand.php
+++ src/Console/Commands/SeedCommand.php
@@ -5,18 +5,18 @@ declare(strict_types=1);
 namespace Misaf\VendraLanguage\Console\Commands;
 
 use Illuminate\Console\Attributes\Description;
+use Illuminate\Console\Attributes\Signature;
 use Misaf\VendraLanguage\Database\Seeders\PermissionPolicySeeder;
 use Misaf\VendraLanguage\LanguagePlugin;
 use Misaf\VendraSupport\Tenancy\Console\Commands\TenantSeedCommand;
 
 #[Description('Seed language module data for a tenant')]
+#[Signature(self::MODULE_NAME.':seed
+        {tenant? : Tenant ID or slug to seed language data for}
+        {seeders?* : Seeder keys to run. Use "all" or one or more of: permission-policies}')]
 final class SeedCommand extends TenantSeedCommand
 {
     protected const string MODULE_NAME = LanguagePlugin::ID;
-
-    protected $signature = self::MODULE_NAME.':seed
-        {tenant? : Tenant ID or slug to seed language data for}
-        {seeders?* : Seeder keys to run. Use "all" or one or more of: permission-policies, demo-contents}';
 
     /**
      * @return array<string, class-string>
